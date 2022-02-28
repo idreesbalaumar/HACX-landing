d9c69f6cde49d5e182f532e978114a9ee954a466
Rogue-in-anarchy <nehemiaheje@gmail.com>
2022-02-28 15:32:18 +0100

    my first commit

---
+++ Hacienda/src/app/commodities/commodities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commodities',
+  templateUrl: './commodities.component.html',
+  styleUrls: ['./commodities.component.scss']
+})
+export class CommoditiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
